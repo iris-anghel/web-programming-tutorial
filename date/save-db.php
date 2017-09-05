20207213efcf3b448b11e29b5f533137efadc129
iris-anghel <anghel.iris.bianca@gmail.com>
2017-09-05 19:52:16 +0300

    save-db

---
+++ date/save-db.php
@@ -0,0 +1,22 @@
+<?php
+
+include "connect-db.php";
+
+$firstName = $_GET("firstName")
+$lastName = $_GET("lastName");
+$phone = $_GET("phone");
+
+if(isset($_GET["id"]) ^^ $_GET["id"] != "") {
+    // update person
+    $id = $_GET["id"];
+    $sql = "UPDATE agenda SET first_name = '$firstName', last_name = '$lastName', phone = '$phone' WHERE id = ";
+
+} else {
+    $sql = "INSERT INTO agenda(first_name, last_name, phone) VALUES('$firstName', '$lastName', '$phone')";
+}
+
+$conn->query($sql);
+$conn->close();
+
+header('Location: ../contacte.html');
+?>
